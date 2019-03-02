06c55800be611f7184cfe5b5e0f704da6b138342
Ethan McCutchen <ethan@grasscommons.org>
2019-03-01 22:59:44 -0500

    convert chainreact to wikirateproject

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Chainreact::Application.load_tasks
+WikirateProject::Application.load_tasks
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-Chainreact::Application.initialize!
+WikirateProject::Application.initialize!
