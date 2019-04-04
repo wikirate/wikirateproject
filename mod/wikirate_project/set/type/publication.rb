4bce459adc3e3c5174fa4652303528f59d4b4405
Ethan McCutchen <ethan@grasscommons.org>
2019-04-04 12:39:25 -0400

    publications support urls

--- mod/wikirate_project/set/type/publication.rb
+++ mod/wikirate_project/set/type/publication.rb
@@ -1,9 +1,16 @@
+card_accessor :uri
+card_accessor :file
+
 format :html do
   view :box_middle do
     field_nest :image, view: :content
   end
 
   view :box_bottom do
-    field_nest :file, view: :core, title: "File"
+    if card.file.present?
+      field_nest :file, view: :core, title: "File"
+    else
+      link_to "Go to Page", href: card.uri
+    end
   end
 end
