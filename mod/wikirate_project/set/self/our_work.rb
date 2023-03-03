e6d149f63e51e0183e46562edb2c86c256e19af3
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-03-03 15:45:21 +0100

    developing our work section further

--- mod/wikirate_project/set/self/our_work.rb
+++ mod/wikirate_project/set/self/our_work.rb
@@ -4,7 +4,7 @@ include_set Abstract::Slider
 
 format :html do
 
-  %i[core].each do |view|
+  %i[core open_data why_open_data].each do |view|
     view view, template: :haml
   end
 
--- mod/wikirate_project/set/type/blurb.rb
+++ mod/wikirate_project/set/type/blurb.rb
@@ -1,11 +1,12 @@
 card_accessor :image, type: :image
+card_accessor :image_attribution, type: :html
 card_accessor :title, type: :phrase
 card_accessor :body, type: :html
 card_accessor :link, type: :uri
 
 format :html do
   def edit_fields
-    %i[image title body link]
+    %i[image image_attribution title body link]
   end
 
   view :core do
@@ -24,9 +25,12 @@ format :html do
     [field_nest(:title, view: :core)]
   end
 
+  view :page, template: :haml
   view :stack, template: :haml, wrap: :slot
   view :head_and_lead, template: :haml
   view :image_left_text_right, template: :haml
+  view :large_image_left_text_right, template: :haml
   view :image_right_text_left, template: :haml
+  view :large_image_right_text_left, template: :haml
   view :action_card, template: :haml
 end
