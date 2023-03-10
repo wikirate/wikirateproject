cb1d049f88b1cf53726620fe9b12a49bccdd0ca1
Ethan McCutchen <ethan@decko.org>
2023-03-10 12:44:47 -0500

    section header fixes

--- mod/wikirate_shared/set/abstract/section_header.rb
+++ mod/wikirate_shared/set/abstract/section_header.rb
@@ -1,5 +1,5 @@
 format :html do
   def section_header blurb, button={}
-    haml :section_header, blurb: blurb, button: button
+    haml :section_header, blurb: blurb, button: button,yielded: (yield if block_given?)
   end
 end
