ca5ec4d6dbd15a3ea3d764b5e577ba3b8969fd70
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-03-14 12:54:59 +0100

    rename playground.js to wikirateproject.js

--- mod/vasso/assets/script/wikirateproject.js
+++ mod/vasso/assets/script/wikirateproject.js
@@ -1,7 +1,4 @@
-// document.addEventListener('click', function (event){
-//     alert('Oh you clicked me!')
-// })
-
+// trigger endorsement modal when clicking on endorsement
 function trigger_endorsement_modal(id) {
     document.querySelector('[endorsement-card-id="' + id + '"]').getElementsByTagName("a")[0].click()
 }
