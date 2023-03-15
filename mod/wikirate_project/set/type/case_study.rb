a5d33baeb880e25fcd961476f56e4d0a14c73420
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-03-16 00:36:47 +0100

    changes on case_study core view

--- mod/wikirate_project/set/type/case_study.rb
+++ mod/wikirate_project/set/type/case_study.rb
@@ -1,11 +1,11 @@
 card_accessor :link, type: :uri
 card_accessor :image, type: :image
 card_accessor :description, type: :basic
-card_accessor :partner, type: :partner
+card_accessor :participants, type: :list
 
 format :html do
   def edit_fields
-    %i[link image description partner]
+    %i[link image description participants]
   end
 
   view :core, template: :haml
