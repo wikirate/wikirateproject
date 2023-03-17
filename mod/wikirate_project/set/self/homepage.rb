3cf698469b2efbffdaa28da974ba6d0d083be84a
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-03-17 18:23:16 +0100

    changes on socials, defining social_description and *title cards

--- mod/wikirate_project/set/self/homepage.rb
+++ mod/wikirate_project/set/self/homepage.rb
@@ -6,11 +6,27 @@ format :html do
   end
 
   def latest_blogs
-    Card.search(type: "Blog", limit: 2, sort_by: 'create', dir: 'desc')
+    Card.search(type: 'Blog', limit: 2, sort_by: 'create', dir: 'desc')
   end
 
   def latest_events
-    Card.search(type: "Event", limit: 2, sort_by: 'create', dir: 'desc')
+    Card.search(type: 'Event', limit: 2, sort_by: 'create', dir: 'desc')
+  end
+
+  def social_image
+    @social_image = 'https://dq06ugkuram52.cloudfront.net/files/79/40132733-large.png'
+  end
+
+  def social_description
+    Card.fetch(:social_description).content
+  end
+
+  def og_site_name
+    'Wikirate International e.V.'
+  end
+
+  def og_title
+    "#{card.name} - Wikirate International e.V."
   end
 
   def endorsements
