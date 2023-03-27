ec201c354e28cea4145bbd53581daa816d9b7d81
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-03-27 17:19:34 +0200

    defining core view for output cardtypes

--- mod/wikirate_project/set/type/output.rb
+++ mod/wikirate_project/set/type/output.rb
@@ -12,6 +12,18 @@ format :html do
     %i[image output_type uri file date description partner]
   end
 
+  def date_to_str
+    if !card.date.nil? && card.date != ''
+      date = ::Date.parse(card.date)
+      month = ::Date::ABBR_MONTHNAMES[date.month]
+      day = date.day <= 9 ? "0#{date.day.to_s}" : date.day.to_s
+      year = date.year
+      "#{day} #{month} #{year}"
+    else
+      nil
+    end
+  end
+
   view :box_top, template: :haml
   view :box_middle, template: :haml
 
