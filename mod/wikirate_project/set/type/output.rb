5b7556d0e01b778b0304349c109857b499d8264f
Vasiliki Gkatziaki <vasgat@gmail.com>
2023-03-28 18:39:25 +0200

    changes on output after moving most of the functionality to wikirate_shared mod (here we kept the code that changes the functionality/view of the output)

--- mod/wikirate_project/set/type/output.rb
+++ mod/wikirate_project/set/type/output.rb
@@ -1,10 +1,3 @@
-card_accessor :image
-card_accessor :description
-card_accessor :uri, type: :uri
-card_accessor :file, type: :file
-card_accessor :output_type, type: :pointer
-
-card_accessor :date, type: :date
 card_accessor :partner, type: :pointer
 
 format :html do
@@ -23,10 +16,6 @@ format :html do
       nil
     end
   end
-
-  view :box_top, template: :haml
-  view :box_middle, template: :haml
-
   view :box_bottom, template: :haml
   view :core, template: :haml
 end
