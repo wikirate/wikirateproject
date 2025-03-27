ac0e8223b5b7fb470d04110579fa7614afcaac21
vasgat <vasgat@gmail.com>
2025-03-27 19:24:51 +0100

    add cards :blog_search, :blog_heading to real.yaml, rename core view of blogs to summary and support different core view

---
+++ mod/wikirate_project/set/self/blog.rb
@@ -0,0 +1,12 @@
+include_set Abstract::Breadcrumbs
+include_set Abstract::SectionHeader
+include_set Abstract::SocialImage
+
+format :html do
+  view :core, template: :haml
+
+  def breadcrumb_title
+    "Blogs"
+  end
+
+end
