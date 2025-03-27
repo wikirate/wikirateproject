a6d3d11dee79a59a868a2459fbc6f4d5a0af6f1f
vasgat <vasgat@gmail.com>
2025-03-27 17:37:13 +0100

    feat: add date to blogs to allow adding past Blogs to the site

--- mod/wikirate_project/set/type/blog.rb
+++ mod/wikirate_project/set/type/blog.rb
@@ -1,11 +1,12 @@
 card_accessor :image, type: :image
+card_accessor :date, type: :date
 card_accessor :description, type: :basic
 card_accessor :link, type: :uri
 card_accessor :blog_type, type: :list
 
 format :html do
   def edit_fields
-    %i[image description link blog_type]
+    %i[image date description link blog_type]
   end
 
   def blog_types
